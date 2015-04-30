1eb4f589b4920c2a1424a61bbd59976503a25411
HitLuca <luca.simonetto.94@gmail.com>
2015-04-30 22:11:02 +0200

    Comunicazione client server e fix vari

--- server/server.c
+++ server/server.c
@@ -16,33 +16,90 @@
 
 #define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
+void parseMessage(char *rawMessage);
+void encapsuleMessage(char *message);
+
+void* authorizationThread(void* arg);
+
+
+int main(int argc,char **argv)
+{
+	//Check se il server è gia avviato
+	if (mkfifo(SERVER_AUTHORIZATION_FIFO,FILE_MODE)!=0)
+	{
+		printf("Server gia presente\n");
+		return 0;
+	}
+	else
+	{
+		printf("Sono l'unico server attivo\n");
+
+		//Creo il thread con la parte di autorizzazione
+		pthread_t authorization;
+		pthread_create (&authorization, NULL, &authorizationThread, NULL);
+
+		mkfifo(SERVER_ANSWER_FIFO,FILE_MODE);
+		int serverAnswerFIFO = open(SERVER_ANSWER_FIFO,O_RDWR);
+
+		//Il server legge le risposte da serverAnswerFIFO
+		char message[MAX_MESSAGE_SIZE];
+		printf("answThread: In lettura:\n");
+		while (1) 
+		{
+			read(serverAnswerFIFO,message,MAX_MESSAGE_SIZE);
+			parseMessage(message);
+			printf("answThread: Ho ricevuto %s nella FIFO risposte\n", message);
+		}
+		return 0;
+	}
+}
 
 void parseMessage(char *rawMessage)
 {
 	*strchr(rawMessage,'!')='\0';
 }
 
-int main(int argc,char **argv)
+void encapsuleMessage(char *message)
 {
-	mkfifo(SERVER_ANSWER_FIFO,FILE_MODE);
-	open(SERVER_ANSWER_FIFO,O_RDWR);
-	if(mkfifo(SERVER_AUTHORIZATION_FIFO,FILE_MODE)==0)
+	message[strlen(message)]='!';
+}
+
+void* authorizationThread(void* arg)
+{
+	//Avvio il monitoraggio delle autorizzazioni
+	int serverAuthFIFO = open(SERVER_AUTHORIZATION_FIFO,O_RDWR);
+	char clientMessage[MAX_MESSAGE_SIZE];
+	char serverMessage[MAX_MESSAGE_SIZE];
+
+	int maxPlayers=1;
+	int actualPlayers=0;
+
+	while(1)
 	{
-		int serverAuthFIFO = open(SERVER_AUTHORIZATION_FIFO,O_RDWR);
-		char message[MAX_MESSAGE_SIZE];
-		read(serverAuthFIFO,message,MAX_MESSAGE_SIZE);
-		parseMessage(message);
+		read(serverAuthFIFO,clientMessage,MAX_MESSAGE_SIZE);
+		parseMessage(clientMessage);
+		printf("authThread: %s mi ha contattato\n", clientMessage);
+
+		//Creo il collegamento alla FIFO del client
 		char fifoPath [MAX_FIFO_NAME_SIZE] = CLIENT_MESSAGE_FIFO;
-		strcat(fifoPath,message);
-		printf("Mando un messaggio a %s\n",message);
+		strcat(fifoPath,clientMessage);
 		int clientMessageFIFO = open(fifoPath,O_RDWR);
-		write(clientMessageFIFO,"1schifoso",strlen("0schifoso"));
 		
+		if (actualPlayers<maxPlayers)
+		{
+			strcpy(serverMessage, "1");
+			encapsuleMessage(serverMessage);
+			actualPlayers++;
+		}
+		else
+		{
+			strcpy(serverMessage, "0");
+			encapsuleMessage(serverMessage);
+		}
+
+		write(clientMessageFIFO, serverMessage, strlen(serverMessage));
+	
 	}
-	else
-	{
-		printf("Server gia presente\n");
-		return 0;
-	}
-	return 0;
+
+	printf("authThread: Raggiunto il numero massimo di giocatori, termino");
 }
