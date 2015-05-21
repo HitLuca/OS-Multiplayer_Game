6ffdfdbbf96e1a2573097e31b80b065560d83889
HitLuca <luca.simonetto.94@gmail.com>
2015-05-21 12:48:39 +0200

    fix launchClients

--- src/test/launchClients.c
+++ src/test/launchClients.c
@@ -10,6 +10,24 @@
 
 int main(int argc,char** argv)
 {
+	char filePath[500];
+    char* c;
+    char* past;
+    printf("%s\n", argv[0]);
+    strcpy(filePath, argv[0]);
+    c=filePath;
+    while(1)
+    {
+        past = c;
+        c=strchr(c, '/');
+        if (c==NULL)
+        {
+            break;
+        }
+        c++;
+    }
+    strcpy(past, "../game/client");
+    printf("%s\n", filePath);
 	int i;
 	for(i=1;i<=atoi(argv[1]);i++)
 	{
@@ -17,7 +35,7 @@ int main(int argc,char** argv)
 		{
 			char index[20];	
 			sprintf(index,"%d",i);
-			execl("../game/client","../game/client",index,(char*)0);
+			execl(filePath, filePath, index,(char*)0);
 		}
 	}
 }
